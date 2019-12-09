2bf7f52c6a06b3f27a79b5e6e16acc8d7ca0f3fb
Runze-Y1 <55113395+Runze-Y1@users.noreply.github.com>
2019-12-09 15:02:37 -0500

    Add files via upload

---
+++ jgw/script.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+  $(".p3").mouseenter(function() {
+    $(".p3").addClass("white");
+    $(".p33").addClass("show white");
+  });
+
+  $(".p3").mouseleave(function() {
+    $(".p3").removeClass("white");
+    $(".p33").removeClass("show white");
+  });
+});
+
+
+
+$(document).ready(function() {
+  $(".p4").mouseenter(function() {
+    $(".p4").addClass("white");
+    $(".p44").addClass("show white");
+  });
+
+  $(".p4").mouseleave(function() {
+    $(".p4").removeClass("white");
+    $(".p44").removeClass("show white");
+  });
+});
+
+
+$(document).ready(function() {
+  $(".p5").mouseenter(function() {
+    $(".p5").addClass("white");
+    $(".p55").addClass("show white");
+  });
+
+  $(".p5").mouseleave(function() {
+    $(".p5").removeClass("white");
+    $(".p55").removeClass("show white");
+  });
+});
+
+
+
+$(document).ready(function() {
+  $(".p6").mouseenter(function() {
+    $(".p6").addClass("white");
+    $(".p66").addClass("show white");
+  });
+
+  $(".p6").mouseleave(function() {
+    $(".p6").removeClass("white");
+    $(".p66").removeClass("show white");
+  });
+});
+
+
+
+
+$(document).ready(function() {
+  $(".p7").mouseenter(function() {
+    $(".p7").addClass("white");
+    $(".p77").addClass("show white");
+  });
+
+  $(".p7").mouseleave(function() {
+    $(".p7").removeClass("white");
+    $(".p77").removeClass("show white");
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+

cfaa3c77dd7bfda803deb1fb9f5d7cafcf2d8c02
Runze-Y1 <55113395+Runze-Y1@users.noreply.github.com>
2022-07-14 04:04:29 -0400

    Add files via upload

---
+++ jgw2/script.js
@@ -0,0 +1,147 @@
+$(document).ready(function() {
+  $(".p3").mouseenter(function() {
+    $(".p3").addClass("white");
+    $(".p33").addClass("show white");
+  });
+
+  $(".p3").mouseleave(function() {
+    $(".p3").removeClass("white");
+    $(".p33").removeClass("show white");
+  });
+});
+
+
+
+$(document).ready(function() {
+  $(".p4").mouseenter(function() {
+    $(".p4").addClass("white");
+    $(".p44").addClass("show white");
+  });
+
+  $(".p4").mouseleave(function() {
+    $(".p4").removeClass("white");
+    $(".p44").removeClass("show white");
+  });
+});
+
+
+$(document).ready(function() {
+  $(".p5").mouseenter(function() {
+    $(".p5").addClass("white");
+    $(".p55").addClass("show white");
+  });
+
+  $(".p5").mouseleave(function() {
+    $(".p5").removeClass("white");
+    $(".p55").removeClass("show white");
+  });
+});
+
+
+
+$(document).ready(function() {
+  $(".p6").mouseenter(function() {
+    $(".p6").addClass("white");
+    $(".p66").addClass("show white");
+  });
+
+  $(".p6").mouseleave(function() {
+    $(".p6").removeClass("white");
+    $(".p66").removeClass("show white");
+  });
+});
+
+
+
+
+$(document).ready(function() {
+  $(".p7").mouseenter(function() {
+    $(".p7").addClass("white");
+    $(".p77").addClass("show white");
+  });
+
+  $(".p7").mouseleave(function() {
+    $(".p7").removeClass("white");
+    $(".p77").removeClass("show white");
+  });
+});
+
+$(document).ready(function() {
+  $(".p8").mouseenter(function() {
+    $(".p8").addClass("white");
+    $(".p88").addClass("show white");
+  });
+
+  $(".p8").mouseleave(function() {
+    $(".p8").removeClass("white");
+    $(".p88").removeClass("show white");
+  });
+});
+
+
+
+$(document).ready(function() {
+  $(".p9").mouseenter(function() {
+    $(".p9").addClass("white");
+    $(".p99").addClass("show white");
+  });
+
+  $(".p9").mouseleave(function() {
+    $(".p9").removeClass("white");
+    $(".p99").removeClass("show white");
+  });
+});
+
+
+$(document).ready(function() {
+  $(".p10").mouseenter(function() {
+    $(".p10").addClass("white");
+    $(".p1010").addClass("show white");
+  });
+
+  $(".p10").mouseleave(function() {
+    $(".p10").removeClass("white");
+    $(".p1010").removeClass("show white");
+  });
+});
+
+
+
+$(document).ready(function() {
+  $(".p11").mouseenter(function() {
+    $(".p11").addClass("white");
+    $(".p1111").addClass("show white");
+  });
+
+  $(".p11").mouseleave(function() {
+    $(".p11").removeClass("white");
+    $(".p1111").removeClass("show white");
+  });
+});
+
+
+
+
+$(document).ready(function() {
+  $(".p12").mouseenter(function() {
+    $(".p12").addClass("white");
+    $(".p1212").addClass("show white");
+  });
+
+  $(".p12").mouseleave(function() {
+    $(".p12").removeClass("white");
+    $(".p1212").removeClass("show white");
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
